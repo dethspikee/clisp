d2b4cf9544b521a26d3ece87a38e08111dc7905b
Damian Tabor <zettie008@gmail.com>
2022-06-07 23:22:19 +0100

    Reformat

--- parsing.c
+++ parsing.c
@@ -1,6 +1,6 @@
+#include "mpc.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 
 /* If we are compiling on Windows compile these functions */
 #ifdef _WIN64
@@ -11,37 +11,53 @@
 static char buffer[BUFFER_SIZE];
 
 /* Fake readline function */
-char* readline(char* prompt) {
+char *readline(char *prompt) {
     fputs(prompt, stdout);
     fgets(buffer, BUFFER_SIZE, stdin);
-    char* cpy = malloc(strlen(buffer) + 1);
+    char *cpy = malloc(strlen(buffer) + 1);
     strcpy(cpy, buffer);
     cpy[strlen(cpy) - 1] = '\0';
     return cpy;
 }
 
 /* Fake add_history function */
-void add_history(char* unused) {}
+void add_history(char *unused) {}
 
 /* Otherwise include the editline headers */
 #else
-#include <editline/readline.h>
 #include <editline/history.h>
+#include <editline/readline.h>
 #endif
 
+int main(int argc, char **argv) {
+    /* Create Some Parsers */
+    mpc_parser_t *Number = mpc_new("number");
+    mpc_parser_t *Operator = mpc_new("operator");
+    mpc_parser_t *Expr = mpc_new("expr");
+    mpc_parser_t *Lispy = mpc_new("lispy");
 
-int main(int argc, char** argv) {
+    /* Define them with the following Language */
+    mpca_lang(MPCA_LANG_DEFAULT,
+              "                                         \
+    number   : /-?[0-9]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' ;                  \
+    expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+    lispy    : /^/ <operator> <expr>+ /$/ ;             \
+  ",
+              Number, Operator, Expr, Lispy);
+
     /* Print Version and Exit Information */
     puts("Clisp Version 0.0.0.0.1");
     puts("Press Ctrl+c to Exit\n");
 
-    while(1) {
-        char* input = readline("clisp> ");
+    while (1) {
+        char *input = readline("clisp> ");
         add_history(input);
 
-        printf("No you're a %s\n", input); 
+        printf("No you're a %s\n", input);
         free(input);
     }
-    
+
+    mpc_cleanup(4, Number, Operator, Expr, Lispy); 
     return 0;
 }
